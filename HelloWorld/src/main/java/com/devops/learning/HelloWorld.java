a3e70b78608739cdb174d0ca565629dd7663b174
Sachin K <vedu2605@gmail.com>
2018-03-24 18:39:18 +0530

    First Commit to git.

---
+++ HelloWorld/src/main/java/com/devops/learning/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.devops.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class HelloWorld {
+ public static void main(String[] args) 
+        {
+ SpringApplication.run(HelloWorld.class, args);
+ }
+}
---
+++ HelloWorld/src/main/java/com/devops/learning/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.devops.learning.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@RestController
+public class HelloWorldController {
+ 
+ @RequestMapping("/")
+ public String welcome() {
+ return "Welcome to Spring Boot Tutorials";
+ }
+ 
+ @RequestMapping("/hello")
+ public String myData() {
+ return "Hello Spring Boot";
+ }
+}
